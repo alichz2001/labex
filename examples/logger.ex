d4b75d8eb045ae8b217192b2ad12127f40852b05
alichz <alichz2001@gmail.com>
2023-03-17 19:45:10 +0330

    add simple examples for dev

---
+++ examples/logger.ex
@@ -0,0 +1,6 @@
+defmodule My.Logger do
+  use Labex
+
+
+
+end
---
+++ examples/structured_logs.ex
@@ -0,0 +1,13 @@
+defmodule My.StructuredLogs do
+  use StructuredLogs
+
+  @spec is_structured_log(map) :: {:y, map} | :n
+  def is_structured_log(log) do
+
+  end
+
+  def handle_structured_log(log) do
+
+  end
+
+end
