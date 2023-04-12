d9b0752ddf628eb09ec84268c8474eda30cd5f56
alichz <alichz2001@gmail.com>
2023-04-12 17:41:13 +0330

    add options schema and use nimble_options to validate

--- lib/labex.ex
+++ lib/labex.ex
@@ -3,23 +3,48 @@ defmodule Labex do
   Documentation for `Labex`.
   """
 
-  defmacro __using__(opts) do
-    conf = Keyword.merge(opts, Application.get_env(:labex, __CALLER__.module, []))
+  options_schema = [
+    mode: [
+      type: {:tuple, [:atom, :keyword_list, {:list, :atom}]},
+      required: :true,
+      default: Labex.FileBackend
+    ],
+    conf: [
+      type: :keyword_list,
+      keys: [
+        path: [
+          type: :string,
+          required: :true
+        ],
+        roller: [
+          type: {:or, [{:tuple, [:atom, {:tuple, [:integer, :atom]}]}]}
+        ],
+        naming_strategy: [
+          type: :atom,
+          default: :timestamp
+        ]
+      ]
+    ]
+  ]
 
-    mode = Keyword.get(conf, :mode) || raise Labex.InvalidModeError
+  defmacro __using__(opts) do
+
+    # config options has more priority
+    opts = Keyword.merge(opts, Application.get_env(:labex, __CALLER__.module, []))
+
+    schema = NimbleOptions.new!(unquote(options_schema))
+    opts = NimbleOptions.validate!(opts, schema)
+
 
     quote location: :keep do
       @behaviour :gen_event
 
-      def init(opts), do: unquote(mode).init(opts, unquote(Macro.escape(conf)))
+      def init(opts), do: unquote(Keyword.get(opts, :mode)).init(opts, unquote(Macro.escape(opts)))
 
-      defdelegate handle_event(event, state), to: unquote(mode)
-
+      defdelegate handle_event(event, state), to: unquote(Keyword.get(opts, :mode))
+      
     end
   end
-
-  defdelegate file(opts), to: Labex.Backends.File, as: :init
-
 
 
 end
