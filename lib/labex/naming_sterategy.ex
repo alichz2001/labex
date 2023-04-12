719165dafedf3aea8ba669e8bce1282036b705cd
alichz <alichz2001@gmail.com>
2023-04-12 17:51:40 +0330

    add naming sterategy.

--- lib/labex/naming_sterategy.ex
+++ lib/labex/naming_sterategy.ex
@@ -1,3 +1,9 @@
-defmodule NamingSterategy do
-  
+defmodule Labex.NamingSterategy do
+
+  @type valid_naming_strategies :: :timestamp
+
+  @spec new_name(valid_naming_strategies, any()) :: String.t()
+  def new_name(sterategy, state \\ [])
+  def new_name(:timestamp, _state), do: (DateTime.utc_now() |> DateTime.to_unix() |> Integer.to_string()) <> ".log"
+
 end
