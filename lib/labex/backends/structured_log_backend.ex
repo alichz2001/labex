53c09f4dac6ca3c2da6057d1a7ab7176aba1f280
alichz <alichz2001@gmail.com>
2023-04-13 18:58:07 +0330

    delete useless module

---
+++ lib/labex/backends/structured_log_backend.ex
@@ -0,0 +1,18 @@
+defmodule Labex.StructuredLogBackend do
+
+  def init(opts) do
+    #open multiple file per struct
+  end
+
+  def parse do
+
+  end
+  def log() do
+
+  end
+
+
+  GLogger.follow("profile_id", "streamer_id")
+  GLogger.unfollow("profile_id", "streamer_id")
+
+end
